41eb7bca000ff94221261a032a0a41b96ff8af3e
dany02 <handany1102@gmail.com>
2025-03-20 23:57:23 +0900

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,104 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import Router from "./Router";
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+@import url('https://fonts.googleapis.com/css2?family=Source+Sans+3:ital,wght@0,200..900;1,200..900&display=swap');
+html, body, div, span, applet, object, iframe,
+h1, h2, h3, h4, h5, h6, p, blockquote, pre,
+a, abbr, acronym, address, big, cite, code,
+del, dfn, em, img, ins, kbd, q, s, samp,
+small, strike, strong, sub, sup, tt, var,
+b, u, i, center,
+dl, dt, dd, ol, ul, li,
+fieldset, form, label, legend,
+table, caption, tbody, tfoot, thead, tr, th, td,
+article, aside, canvas, details, embed, 
+figure, figcaption, footer, header, hgroup, 
+menu, nav, output, ruby, section, summary,
+time, mark, audio, video {
+	margin: 0;
+	padding: 0;
+	border: 0;
+	font-size: 100%;
+	font: inherit;
+	vertical-align: baseline;
+}
+/* HTML5 display-role reset for older browsers */
+article, aside, details, figcaption, figure, 
+footer, header, hgroup, menu, nav, section {
+	display: block;
+}
+body {
+	line-height: 1;
+}
+ol, ul {
+	list-style: none;
+}
+blockquote, q {
+	quotes: none;
+}
+blockquote:before, blockquote:after,
+q:before, q:after {
+	content: '';
+	content: none;
+}
+table {
+	border-collapse: collapse;
+	border-spacing: 0;
+}
+body {
+    font-family: "Source Sans 3", sans-serif;
+    background-color: ${(props) => props.theme.bgColor};
+    color: ${(props) => props.theme.textColor};
+}
+a {
+    text-decoration:none;
+    color: inherit;
+}
+* {
+    box-sizing:border-box;
+}
+`;
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <>
+            <GlobalStyle/>
+            <Router/>
+        </>
+    )
+    
+    // const Container = styled.div`
+    //     background-color: ${(props) => props.theme.bgColor};
+    // `;
+    // const H1 = styled.h1`
+    //     color: ${(props) => props.theme.textColor};
+    // `;
+    // return (
+    //     <Container>
+    //         <H1>protected</H1>
+    //     </Container>
+    // );
+    // const [value, setValue] = useState("");
+    // const onChange = (event: React.FormEvent<HTMLInputElement>) => {
+    //     const {currentTarget: {value},} = event;
+    //     setValue(value);
+    // };
+
+    // const onSubmit = (event: React.FormEvent<HTMLFormElement>) =>{
+    //     event.preventDefault();
+
+    // };
+
+    // return (
+    //     <div>
+    //         <form onSubmit={onSubmit}>
+    //             <input onChange={onChange} value={value} type="text" placeholder="username" />
+    //             <button>Login</button>
+    //         </form>
+    //     </div>
+    // );
 }
 
 export default App;
---
+++ src/Circle.tsx
@@ -0,0 +1,28 @@
+import { useState } from "react";
+import styled from "styled-components";
+
+interface CircleProps {
+    $bgColor : string;
+    $borderColor ?: string;
+    text ?: string;
+}
+
+const Container = styled.div<CircleProps>`
+    width:200px;
+    height:200px;
+    background-color: ${props => props.$bgColor};
+    border-radius:100px;
+    border: 1px solid ${props => props.$borderColor};
+`;
+
+
+
+function Circle({$bgColor, $borderColor, text="Default text"}:CircleProps) {
+    const [value, setValue] =useState("");
+    
+    return <Container $bgColor={$bgColor} $borderColor={$borderColor ?? $bgColor}>
+        {text}
+    </Container>;
+}
+
+export default Circle;
